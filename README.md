9157d313b9d4935eafdd09f411b525593e195b64
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2020-02-04 00:37:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# TypeCheat
+A JS script to cheat on certain online typing tests.
+
+## Supported sites
+- [10FastFingers](https://10fastfingers.com/)
+<!-- - [Ratatype](https://www.ratatype.com/) -->
+<!-- ^^^WIP^^^ -->
+I am planning to add support for more.
+
+## Use
+0. Ensure that you're on a supported site
+1. Copy [the contents of `typecheat.min.js`](https://raw.githubusercontent.com/MysteryBlokHed/TypeCheat/master/typecheat.min.js).
+2. Open the developer console (Ctrl+Shift+J on Chrome/Chromium Edge)
+3. Paste the code
+4. The text box will start to autofill the next words as they appear. Simply press `space` as fast as you would like, and the next words will appear as you do.
+
+**Note: On 10FastFingers, if you end up going through the entire text, the words will continue to autofill. If you continue to press space it can cause you to get incorrect words marked, despite the text being over.**
---
+++ typecheat.js
@@ -0,0 +1,72 @@
+/*
+ *    TypeCheat - A JS script to cheat on certain online typing tests
+ *    Copyright (C) 2020  Adam Thompson-Sharpe
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+// Check current site
+let url = window.location.href.split("/")[2];
+let site = -1;
+if(url == "10fastfingers.com" || url == "www.10fastfingers.com") {
+    console.log("~~~~~~~~~~~~~~~~~~~");
+    console.log(" TypeCheat Active! ");
+    console.log("Site: 10FastFingers");
+    console.log("~~~~~~~~~~~~~~~~~~~");
+    site = 0;
+}
+// ---------- WIP ----------
+// else if(url == "www.ratatype.com"|| url == "ratatype.com") {
+//     console.log("~~~~~~~~~~~~~~~~~~~");
+//     console.log(" TypeCheat Active! ");
+//     console.log("Site:      Ratatype");
+//     console.log("~~~~~~~~~~~~~~~~~~~");
+//     site = 1;
+// }
+
+// Start cheating
+if(site == 0) {
+    // Interval
+    let cWord = 0;
+    // Make sure to constantly update the field
+    setInterval(function() {
+        if(document.getElementById("inputfield").value == "") {
+            document.getElementById("inputfield").value = words[cWord];
+            cWord++;
+        }
+    }, 10);
+}
+// ---------- WIP ----------
+// else if(site == 1) {
+//     // Generate wordlist
+//     let text = document.getElementsByClassName("mainTxt")[0];
+//     let field = document.getElementsByClassName("divTextarea")[0].children[0];
+//     let words = [];
+//     for(let i = 0; i < text.children.length; i++) {
+//         if(text.children[i].innerText != " ") {
+//             words.push(text.children[i].innerText);
+//         }
+//     }
+//     console.log(words);
+//     let i = 1;
+//     field.value = words[0];
+//     // Make sure to constantly update the field
+//     setInterval(function() {
+//         if(field.value.slice(-1) == " " || !field.value.includes(words[i])) {
+//             field.value += words[i];
+//             // Make sure that the word was actually added to the text field
+//             if(field.value.includes(words[i])) i++;
+//             console.log(field.value.includes(words[i]));
+//         }
+//     }, 10);
+// }
