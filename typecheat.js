cfb45dbd3caffc93cb471db247baa0ffd673dd11
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2020-02-04 15:37:13 -0500

    Fixed problems on 10FastFingers that stopped the script from working.

--- typecheat.js
+++ typecheat.js
@@ -19,23 +19,14 @@
 let url = window.location.href.split("/")[2];
 let site = -1;
 if(url == "10fastfingers.com" || url == "www.10fastfingers.com") {
-    console.log("~~~~~~~~~~~~~~~~~~~");
-    console.log(" TypeCheat Active! ");
-    console.log("Site: 10FastFingers");
-    console.log("~~~~~~~~~~~~~~~~~~~");
+    console.log("~~~~~~~~~~~~~~~~~~~\n TypeCheat Active! \nSite: 10FastFingers\n~~~~~~~~~~~~~~~~~~~");
     site = 0;
 // ---------- WIP ----------
 // } else if(url == "www.ratatype.com"|| url == "ratatype.com") {
-//     console.log("~~~~~~~~~~~~~~~~~~~");
-//     console.log(" TypeCheat Active! ");
-//     console.log("Site:      Ratatype");
-//     console.log("~~~~~~~~~~~~~~~~~~~");
+//     console.log("~~~~~~~~~~~~~~~~~~~\n TypeCheat Active! \nSite:      Ratatype\n~~~~~~~~~~~~~~~~~~~");
 //     site = 1;
 } else if(url == "typingtestnow.com" || url == "www.typingtestnow.com") {
-    console.log("~~~~~~~~~~~~~~~~~~~");
-    console.log(" TypeCheat Active! ");
-    console.log("Site: TypingTestNow");
-    console.log("~~~~~~~~~~~~~~~~~~~");
+    console.log("~~~~~~~~~~~~~~~~~~~\n TypeCheat Active! \nSite: TypingTestNow\n~~~~~~~~~~~~~~~~~~~");
     site = 2;
 }
 
@@ -43,41 +34,49 @@ if(url == "10fastfingers.com" || url == "www.10fastfingers.com") {
 switch(site) {
     // 10FastFingers
     case 0:
+        // Generate wordlist
+        let tText = document.getElementById("row1");
+        let tWords = [];
+        for(let i = 0; i < tText.children.length; i++) {
+            tWords.push(tText.children[i].innerText);
+        }
         // Interval
         let cWord = 0;
         // Make sure to constantly update the field
         setInterval(function() {
             if(document.getElementById("inputfield").value == "") {
-                document.getElementById("inputfield").value = words[cWord];
+                document.getElementById("inputfield").value = tWords[cWord];
                 cWord++;
             }
         }, 10);
         break;
     // ---------- WIP ----------
-    // // Ratatype
-    // case 1:
-    //     // Generate wordlist
-    //     let text = document.getElementsByClassName("mainTxt")[0];
-    //     let field = document.getElementsByClassName("divTextarea")[0].children[0];
-    //     let words = [];
-    //     for(let i = 0; i < text.children.length; i++) {
-    //         if(text.children[i].innerText != " ") {
-    //             words.push(text.children[i].innerText);
-    //         }
-    //     }
-    //     let i = 1;
-    //     field.value = words[0];
-    //     // Make sure to constantly update the field
-    //     setInterval(function() {
-    //         if(field.value.slice(-1) == " " || !field.value.includes(words[i])) {
-    //             field.value += words[i];
-    //             // Make sure that the word was actually added to the text field
-    //             if(field.value.includes(words[i])) i++;
-    //             console.log(field.value.includes(words[i]));
-    //         }
-    //     }, 10);
-    //     break;
-    // // TypingTestNow
+    // Ratatype
+    /*
+     * case 1:
+     *     // Generate wordlist
+     *     let text = document.getElementsByClassName("mainTxt")[0];
+     *     let field = document.getElementsByClassName("divTextarea")[0].children[0];
+     *     let words = [];
+     *     for(let i = 0; i < text.children.length; i++) {
+     *         if(text.children[i].innerText != " ") {
+     *             words.push(text.children[i].innerText);
+     *         }
+     *     }
+     *     let i = 1;
+     *     field.value = words[0];
+     *     // Make sure to constantly update the field
+     *     setInterval(function() {
+     *         if(field.value.slice(-1) == " " || !field.value.includes(words[i])) {
+     *             field.value += words[i];
+     *             // Make sure that the word was actually added to the text field
+     *             if(field.value.includes(words[i])) i++;
+     *             console.log(field.value.includes(words[i]));
+     *         }
+     *     }, 10);
+     *      break;
+     */
+    // TypingTestNow
     case 2:
         // Generate wordlist
         let text = document.getElementsByClassName("sample-text")[0];
@@ -87,7 +86,6 @@ switch(site) {
                 words.push(text.children[i].children[j].innerText);
             }
         }
-        console.log(words);
         // Interval
         let i = 0;
         // Make sure to constantly update the field
