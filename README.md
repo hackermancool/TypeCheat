2f8e19d9299059452cba1894a4157e3492a1c936
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2020-02-10 18:46:41 -0500

    Updated Tampermonkey version of the script.

--- README.md
+++ README.md
@@ -22,15 +22,19 @@ If you go above a certain WPM (not sure exactly what), you will be forced to tak
 2. Open the developer console (Ctrl+Shift+J on Chrome/Chromium Edge)
 3. Paste the code and press enter
 4. The text box will start to autofill the next words as they appear. Simply press `space` as fast as you would like, and the next words will appear as you do.
+
 ### Tampermonkey
 If you have the [Tampermonkey Extension](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) installed:
 
 1. Click on the tampermonkey icon next to the search bar.
 2. Select "Create a new script..."
-3. Copy [the contents of `typecheat.tampermonkey.js`](https://raw.githubusercontent.com/MysteryBlokHed/TypeCheat/master/typecheat.tampermonkey.js).
-4. Replace the default contents with the contents of `typecheat.tampermonkey.js` (Ctrl+A, then Ctrl+V).
+3. Copy [the contents of `typecheat-tampermonkey.min.js`](https://raw.githubusercontent.com/MysteryBlokHed/TypeCheat/master/typecheat-tampermonkey.min.js).
+4. Replace the default contents with the contents of `typecheat-tampermonkey.min.js` (While inside the editor, Ctrl+A then Ctrl+V).
 5. Save the script (Ctrl+S).
 
-The script will now autorun on supported sites.
+Once you're on a supported site, you should see an button labelled "Start TypeCheat". Click this only when the words have finished loading. The text of the button will appear green when it is active.
+
+On **10FastFingers**, the button appears just **below** the text field.  
+On **TypingTestNow**, it appears at the **top of the page** just under the navbar.
 
 **Note: On 10FastFingers, if you end up going through the entire text, the words will continue to autofill. If you continue to press space it can cause you to get incorrect words marked, despite the text being over.**
---
+++ typecheat-tampermonkey.js
@@ -0,0 +1,121 @@
+// ==UserScript==
+// @name         TypeCheat
+// @namespace    https://github.com/MysteryBlokHed/TypeCheat
+// @version      v1.0.0-beta.2
+// @description  A JS script to cheat on certain online typing tests.
+// @author       Adam Thompson-Sharpe
+// @match        https://10fastfingers.com/*
+// @match        https://www.ratatype.com/*
+// @match        https://typingtestnow.com/*
+// @grant        none
+// ==/UserScript==
+/*
+ *    TypeCheat - A JS script to cheat on certain online typing tests
+ *    Copyright (C) 2020  Adam Thompson-Sharpe
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+/*
+ *    TypeCheat - A JS script to cheat on certain online typing tests
+ *    Copyright (C) 2020  Adam Thompson-Sharpe
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+// Check current site
+let url = window.location.href.split("/")[2];
+let site = -1;
+let altMode = 0;
+if(url == "10fastfingers.com" || url == "www.10fastfingers.com") {
+    console.log("~~~~~~~~~~~~~~~~~~~\n TypeCheat Active! \nSite: 10FastFingers\n~~~~~~~~~~~~~~~~~~~");
+    site = 0;
+    if(window.location.href.split("/")[3] == "text") altMode = 1;
+} else if(url == "typingtestnow.com" || url == "www.typingtestnow.com") {
+    console.log("~~~~~~~~~~~~~~~~~~~\n TypeCheat Active! \nSite: TypingTestNow\n~~~~~~~~~~~~~~~~~~~");
+    site = 2;
+}
+
+// 10FastFingers Cheat Code
+function tenFastCheat() {
+    // Different method for different modes
+    let tField = null;
+    if(altMode == 0) {
+        tField = document.getElementById("inputfield");
+    } else if(altMode == 1) {
+        tField = document.getElementById("text_typed");
+    }
+    tField.addEventListener("keyup", function(e) {
+        if(e.keyCode == 32) {
+            tField.value = tWords[cWord];
+            cWord++;
+        }
+    });
+    // Change button colour to show that it has activated
+    document.getElementById("ads-speedtest-view-container").children[0].style = "width: 100%; color: #0F0;";
+}
+
+// TypingTestNow Cheat Code
+function typingTestNowCheat() {
+    let field = document.getElementById("practice-input");
+    field.addEventListener("keydown", function(e) {
+        if(e.keyCode == 32) {
+            field.value = words[i];
+            i++;
+        }
+    });
+    // Change button colour to show that it has activated
+    document.getElementsByClassName("row")[0].children[0].style = "width: 100%; color: #0F0;";
+}
+
+// Cheat
+switch(site) {
+    // 10FastFingers (Normal)
+    case 0:
+        // Generate wordlist
+        var tText = document.getElementById("row1");
+        var tWords = [];
+        for(let i = 0; i < tText.children.length; i++) {
+            tWords.push(tText.children[i].innerText);
+        }
+        // Interval
+        var cWord = 0;
+        // Create cheat button
+        document.getElementById("ads-speedtest-view-container").innerHTML = "<button style=\"width: 100%;\" onclick=\"javascript:tenFastCheat();\">Start TypeCheat</button>";
+        break;
+    // TypingTestNow
+    case 2:
+        // Generate wordlist
+        let text = document.getElementsByClassName("sample-text")[0];
+        var words = [];
+        for(let i = 0; i < text.children.length; i++) {
+            for(let j = 0; j < text.children[i].children.length; j++) {
+                words.push(text.children[i].children[j].innerText);
+            }
+        }
+        // Interval
+        var i = 0;
+        // Create cheat button
+        document.getElementsByClassName("row")[0].innerHTML = "<button style=\"width: 100%;\" onclick=\"javascript:typingTestNowCheat();\">Start TypeCheat</button>"
+        break;
+}
